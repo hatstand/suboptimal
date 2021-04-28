6708029796e1dc5bb9c0ef74bc4b3c0d692128a1
John Maguire <john.maguire@gmail.com>
2021-04-28 21:23:58 +0100

    Fetch and parse real flags file

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,56 @@
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
-#[derive(Serialize, Deserialize)]
-struct OptimizelyFile {
-    version: String,
+#[derive(Serialize, Deserialize, Debug)]
+struct TrafficAllocation {
+    #[serde(rename="entityId")]
+    entity_id: String,
+    #[serde(rename="endOfRange")]
+    end_of_range: u64,
 }
 
-fn main() {
-    let data = r#"
-    {
-        "version": "42"
-    }"#;
+#[derive(Serialize, Deserialize, Debug)]
+struct FeatureFlag {
+    key: String,
+    #[serde(rename="rolloutId")]
+    rollout_id: String,
+}
 
-    let p: Result<OptimizelyFile> = serde_json::from_str(data);
-    match p {
-        Ok(f) => println!("{}", f.version),
+#[derive(Serialize, Deserialize, Debug)]
+struct Experiment {
+    id: String,
+    key: String,
+    #[serde(rename="layerId")]
+    layer_id: String,
+    #[serde(rename="trafficAllocation")]
+    traffic_allocation: Vec<TrafficAllocation>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Rollout {
+    id: String,
+    experiments: Vec<Experiment>,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct OptimizelyFile {
+    version: String,
+    rollouts: Vec<Rollout>,
+    #[serde(rename="featureFlags")]
+    feature_flags: Vec<FeatureFlag>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://flags.creditkudos.com/Production.json")
+        .await?
+        .text()
+        .await?;
+
+    match serde_json::from_str::<OptimizelyFile>(&resp) {
+        Ok(f) => println!("{:#?}", f),
         Err(e) => println!(":-( {}", e),
     }
+
+    Ok(())
 }
